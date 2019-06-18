db66351fd10b3ff0e97fe07fe7a2caddc3525259
Paliwal Gourav - (Service Transformation) <PA20068581@wipro.com>
2019-06-18 16:53:05 +0530

    cleanup

--- TradeApp/dltservice/app.js
+++ TradeApp/dltservice/app.js
@@ -16,7 +16,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/trades',tradesRouter);
 
 module.exports = app;
--- TradeApp/truffle-config.js
+++ TradeApp/truffle-config.js
@@ -9,7 +9,7 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*"
+      network_id: "ganache"
     }/*,
     test: {
       host: "127.0.0.1",
