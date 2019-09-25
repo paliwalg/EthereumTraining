06fbef04d67cdfee8916fb2dd773c33b6f13245b
paliwalg <paliwal.gourav@wipro.com>
2019-09-25 12:59:20 +0530

    Update TradeInterface.sol

--- TradeApp/contracts/TradeInterface.sol
+++ TradeApp/contracts/TradeInterface.sol
@@ -1,12 +1,59 @@
 pragma solidity >=0.5.1 <0.6.0;
 
+/**
+* @title Required interface for Trade related operations.
+*/
 interface TradeInterface {
     
-    event TradeCreated(bytes32 _tradeId, address _fromParty, address _toParty, uint _amount, uint _tradeDate);
+    // Event fired after trade is added into the mapping
+    event TradeCreated(
+        bytes32 _tradeId, 
+        address _fromParty, 
+        address _toParty,
+        address _owner,
+        uint _amount, 
+        uint _tradeDate);
+    
+    /**
+     * @dev Adds a trade into mapping tradeList
+     * _tradeId should be unique and it is used as key to store tradeStruct data into mapping 
+     * @param _tradeId : unique id for a particular trade
+     * @param _fromParty : address of sender
+     * @param _toParty : address of recepient 
+     * @param _amount : trade amount
+     * @param _tradeDate : trade date in unixtimestamp 
+     */
+    function  addTrade(
+        bytes32 _tradeId, 
+        address _fromParty, 
+        address _toParty, 
+        uint _amount, 
+        uint _tradeDate) external;
 
-    function  addTrade(bytes32 _tradeId, address _fromParty, address _toParty, uint _amount, uint _tradeDate) external;
+    /**
+     * @dev Fetches a particular trade from mapping tradeList based on the _tradeId
+     * @param _tradeId : unique trade id of a particular trade
+     * @return bytes32 : trade id
+     * @return address : address of sender
+     * @return address : address of recepient
+     * @return address : address of owner
+     * @return uint : trade amount
+     * @return uint : trade date in unixtimestamp
+     */
+    function getTrade(
+        bytes32 _tradeId) external view returns (bytes32,
+        address,
+        address,
+        address,
+        uint,
+        uint);
 
-    function getTrade(bytes32 _tradeId) external view returns (address, address, uint, uint);
-
+    /**
+     * @dev Adds a trade into mapping tradeList
+     * @param _tradeId : unique trade id of a particular trade
+     * @param _amount : trade amount 
+     */
+    function updateTrade(
+       bytes32 _tradeId,
+        uint _amount) external;
 }
-
